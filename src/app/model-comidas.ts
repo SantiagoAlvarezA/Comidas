121e31beac270d4b526f48d45abe0134e1090522
Santiago Alvarez <aalvarez3570@gmail.com>
2018-10-09 13:54:02 -0500

    imagen en modal

--- src/app/model-comidas.ts
+++ src/app/model-comidas.ts
@@ -1,14 +1,13 @@
 import { TypeComidas } from './type-comidas';
 export const COMIDAS: TypeComidas[] = [
-    { id: 1, name: 'Sandwich', url: 'https://cdn.pixabay.com/photo/2013/07/12/14/13/sandwich-148023_960_720.png', cost: 10000 },
-    { id: 2, name: 'Salchipapas', url: 'https://cdn.pixabay.com/photo/2016/03/31/20/31/chips-1295837_960_720.png', cost: 10000 },
-    { id: 3, name: 'Hamburguesa', url: 'https://cdn.pixabay.com/photo/2012/04/13/01/51/hamburger-31775_960_720.png', cost: 10000 },
-
-    { id: 4, name: 'Ensalada de frutas', url: 'https://cdn.pixabay.com/photo/2014/12/21/23/40/pineapple-boat-575802_960_720.png', cost: 10000 },
-    { id: 5, name: 'Perro caliente', url: 'https://cdn.pixabay.com/photo/2014/04/02/11/02/hot-305302_960_720.png', cost: 10000 },
-    { id: 6, name: 'Toston', url: 'http://www.doralzueladelivery.com/wp-content/uploads/2016/08/PC-79A.jpg', cost: 10000 },
-    { id: 7, name: 'Malteadas', url: 'https://d701vexhkz032.cloudfront.net/media/images/menu-content/CO/postres/Malteadas_produto_new_co.png', cost: 10000 },
-    { id: 8, name: 'Pizza', url: 'https://cdn.pixabay.com/photo/2013/07/13/09/36/pizza-155771_960_720.png', cost: 10000 },
-    { id: 9, name: 'McColoso Mora Arequipe', url: 'https://d701vexhkz032.cloudfront.net/media/images/menu-content/CO/postres/sundae-mora.png', cost: 10000 },
-    { id: 10, name: 'Granizado', url: 'https://cdn.pixabay.com/photo/2013/07/12/15/21/iced-coffee-149720_960_720.png', cost: 10000 }
+    { id: 1, name: 'Sandwich', url: 'https://cdn.pixabay.com/photo/2013/07/12/14/13/sandwich-148023_960_720.png', cost: 1500 },
+    { id: 2, name: 'Salchipapas', url: 'https://cdn.pixabay.com/photo/2016/03/31/20/31/chips-1295837_960_720.png', cost: 2500 },
+    { id: 3, name: 'Hamburguesa', url: 'https://cdn.pixabay.com/photo/2012/04/13/01/51/hamburger-31775_960_720.png', cost: 14000 },
+    { id: 4, name: 'Ensalada de frutas', url: 'https://cdn.pixabay.com/photo/2014/12/21/23/40/pineapple-boat-575802_960_720.png', cost: 12000 },
+    { id: 5, name: 'Perro caliente', url: 'https://cdn.pixabay.com/photo/2014/04/02/11/02/hot-305302_960_720.png', cost: 6000 },
+    { id: 6, name: 'Toston', url: 'http://www.doralzueladelivery.com/wp-content/uploads/2016/08/PC-79A.jpg', cost: 11000 },
+    { id: 7, name: 'Malteadas', url: 'https://d701vexhkz032.cloudfront.net/media/images/menu-content/CO/postres/Malteadas_produto_new_co.png', cost: 5000 },
+    { id: 8, name: 'Pizza', url: 'https://cdn.pixabay.com/photo/2013/07/13/09/36/pizza-155771_960_720.png', cost: 6500 },
+    { id: 9, name: 'McColoso Mora Arequipe', url: 'https://d701vexhkz032.cloudfront.net/media/images/menu-content/CO/postres/sundae-mora.png', cost: 5600 },
+    { id: 10, name: 'Granizado', url: 'https://cdn.pixabay.com/photo/2013/07/12/15/21/iced-coffee-149720_960_720.png', cost: 1000 }
 ];
